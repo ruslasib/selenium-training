acb61cf28ad1bbba757d48f9b3debdfe205b85cc
Ruslan Sibgatullin <rusla.sib@gmail.com>
2020-11-10 23:15:03 +0300

    Initial commit

---
+++ selenium-training/src/test/java/ru/ruslasib/training/selenium/MyFirstTest.java
@@ -0,0 +1,32 @@
+package ru.ruslasib.training.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstTest {
+
+  private WebDriver driver;
+
+  @Before
+  public void start() {
+    driver = new ChromeDriver();
+  }
+
+  @Test
+  public void myFirstTest() {
+    driver.get("http://www.google.com/");
+    driver.findElement(By.name("q")).sendKeys("webdriver");
+    driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+  }
+
+  @After
+  public void stop() {
+    driver.quit();
+    driver = null;
+  }
+}
