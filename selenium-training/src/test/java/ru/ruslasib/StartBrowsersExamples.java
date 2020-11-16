ad3ae717ce61f15af7be0c29f5a8d5bb8d0e1856
Ruslan Sibgatullin <rusla.sib@gmail.com>
2020-11-16 20:43:53 +0300

    Launching different browsers: some comments

--- selenium-training/src/test/java/ru/ruslasib/StartBrowsersExamples.java
+++ selenium-training/src/test/java/ru/ruslasib/StartBrowsersExamples.java
@@ -9,8 +9,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeDriverService;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
 
@@ -18,12 +20,28 @@ public class StartBrowsersExamples {
 
   private WebDriver driver;
 
+  // Предпочтительно путь до драйвера прописывать в переменных окружения PATH
   @Test
   public void startChrome() {
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setCapability("strictFileInteractability", true);
     ChromeOptions options = new ChromeOptions();
     options.addArguments("start-maximized");
+    capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+
+    // использовать этот конструктор нежелательно, так как он deprecated
+    // driver = new ChromeDriver(capabilities);
+
+    // лучше использовать конструктор с ChromeOptions
     driver = new ChromeDriver(options);
-    System.out.println(((HasCapabilities) driver).getCapabilities());
+    doSomething();
+  }
+
+  // Путь до драйвера можно положить куда угодно на диск, указав путь до него явно в коде
+  @Test
+  public void startChromeWhenDriverSomewhereInOS() {
+    System.setProperty("webdriver.chrome.driver", "d:\\Software_testing\\Distrib\\chromedriver.exe");
+    driver = new ChromeDriver();
     doSomething();
   }
 
@@ -31,18 +49,17 @@ public class StartBrowsersExamples {
   public void startFirefox() {
     FirefoxOptions options = new FirefoxOptions();
     options.addArguments();driver = new FirefoxDriver(options);
-    System.out.println(((HasCapabilities) driver).getCapabilities());
     doSomething();
   }
 
   @Test
   public void startEdge() {
-    // код, который находит драйвер Edge
-    // driver = new EdgeDriver(new EdgeDriverService.Builder()
-    //        .usingDriverExecutable(new File("C:\\Tools\\msedgedriver.exe")).build());
-
-    // код, который не находит драйвер Edge
-    driver = new EdgeDriver();
+    // у меня браузер Edge запустился только, когда явно указан путь до веб драйвера
+    driver = new EdgeDriver(new EdgeDriverService.Builder()
+            .usingDriverExecutable(new File("C:\\Tools\\msedgedriver.exe")).build());
+    // а вот этот код для Edge не сработал. Путь до драйвера был прописан в PATH
+    // для Chrome и Firefox похожий код сработал без явного указания пути до драйвера
+    // driver = new EdgeDriver();
     doSomething();
   }
 
@@ -53,6 +70,7 @@ public class StartBrowsersExamples {
   }
 
   public void doSomething() {
+    System.out.println(((HasCapabilities) driver).getCapabilities());
     driver.get("http://www.google.com/");
     driver.findElement(By.name("q")).sendKeys("webdriver");
     driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
